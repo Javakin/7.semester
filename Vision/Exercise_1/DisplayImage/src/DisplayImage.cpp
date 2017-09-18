6de6f303b92397e3db004270fb8ad995ffcaf753
Javakin <danielharaldson@yahoo.com>
2017-09-18 16:36:04 +0200

    skrevet lidt i guess

--- Vision/Exercise_1/DisplayImage/src/DisplayImage.cpp
+++ Vision/Exercise_1/DisplayImage/src/DisplayImage.cpp
@@ -33,7 +33,6 @@ int main()
     }
 
     // 1. different methods for drawing black rectangles
-    Mat img2 = clone(img);
     rect1();
     rect2();
     rect3();
@@ -41,12 +40,17 @@ int main()
     // 2. draw a collered rectangle
     rectColor();
 
-    // 3. comvert the image to grayscale
-    grayScale();
 
-    // Show the images
-    cv::imshow("Image", img2);
-    cv::waitKey();
+    // 3. comvert the image to grayscale
+    cv::Mat graySC = cv::imread("../color.png", CV_LOAD_IMAGE_GRAYSCALE);
+
+        // display image
+    cv::imshow("Image", graySC);
+   // cv::waitKey();
+
+
+
+
 
     // save the images
 
@@ -56,26 +60,49 @@ int main()
 
 //function definition
 void rect1(){
-    cout << "stuff - tjis is function 1" << endl;
+    //rectangle drawn using rectangle draw funciton
 }
 
 
 void rect2(){
-    cout << "stuff - tjis is function 2" << endl;
+    // rectangle drawn using line draw function
+
 }
 
 
 void rect3(){
-    cout << "stuff - this is function 3" << endl;
+    // rectangle drawn using point manipulation functions
+    cv::Mat img = cv::imread("../color.png");
+    cv::Mat img2 = img.clone();
+
+
+    for (unsigned int i = 7; i<=70; i++){
+        for(unsigned int j = 8; j<=80; j++){
+            cout << i << j<< endl;
+            //img.at<cv::Vec3b>(i, j) = cv::Scalar::all(0);
+        }
+    }
+    Vec3b intensity = img.at<Vec3b>(10, 10);
+    uchar blue = intensity.val[0];
+    uchar green = intensity.val[1];
+    uchar red = intensity.val[2];
+
+    cout << "blue" << (int)blue << " green" << (int)green << " red" << (int)red << endl;
+
+    // save the image in a file
+    cv::imwrite("../Images/rect3.png",img2);
+    cv::imshow("Image58",img);
+    cv::waitKey();
+
+
 }
 
 void rectColor(){
+    // todo
 
 }
 
-void grayScale(){
 
-}
 
 
 

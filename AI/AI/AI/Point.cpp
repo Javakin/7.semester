3e6f3690f2291ae23c5e203702798b1c73a5ac27
Javakin <danielharaldson@yahoo.com>
2017-10-09 19:44:49 +0200

    så er point klassen skrevet færdig

--- AI/AI/AI/Point.cpp
+++ AI/AI/AI/Point.cpp
@@ -27,6 +27,38 @@ int& Point::y()
 
 
 
+Point Point::operator+(Point p2)
+{
+	Point res;
+	res.x() = x_val + p2.x();
+	res.y() = y_val + p2.y();
+	return res;
+}
+
+Point Point::operator-(Point p2)
+{
+	Point res;
+	res.x() = x_val - p2.x();
+	res.y() = y_val - p2.y();
+	return res;
+}
+
+Point Point::operator+=(int i)
+{
+	Point res;
+	res.x() = x_val + i;
+	res.y() = y_val + i;
+	return res;
+}
+
+Point Point::operator-=(int i)
+{
+	Point res;
+	res.x() = x_val - i;
+	res.y() = y_val - i;
+	return res;
+}
+
 Point::~Point()
 {
 }
--- AI/AI/AI/Point.h
+++ AI/AI/AI/Point.h
@@ -13,7 +13,10 @@ public:
 	
 
 	// operator overloads +, -, +=, -=
-	
+	Point operator+  (Point p2);
+	Point operator-  (Point p2);
+	Point operator+= (int i);
+	Point operator-= (int i);
 
 	// destructors
 	~Point();
@@ -25,33 +28,4 @@ private:
 };
 
 
-class Box {
-public:
-	double getVolume(void) {
-		return length * breadth * height;
-	}
-	void setLength(double len) {
-		length = len;
-	}
-	void setBreadth(double bre) {
-		breadth = bre;
-	}
-	void setHeight(double hei) {
-		height = hei;
-	}
-
-	// Overload + operator to add two Box objects.
-	Box operator+(const Box& b) {
-		Box box;
-		box.length = this->length + b.length;
-		box.breadth = this->breadth + b.breadth;
-		box.height = this->height + b.height;
-		return box;
-	}
-
-private:
-	double length;      // Length of a box
-	double breadth;     // Breadth of a box
-	double height;      // Height of a box
-};
 
